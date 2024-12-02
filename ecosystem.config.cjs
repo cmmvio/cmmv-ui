aded3638ddaa811dfec5bbc8e146448a592b3904
Andre <andre@tos.world>
2024-12-02 18:34:53 -0300

    fix: package

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,13 @@
 module.exports = {
-    apps : [{
-        name   : "UI",
-        script : "pnpm run preview"
-    }]
-}
+    apps: [
+        {
+            name: "UI",
+            script: "pnpm",
+            args: "run preview",
+            interpreter: "/root/.nvm/versions/node/v22.8.0/bin/node", 
+            env: {
+                NODE_ENV: "production"
+            }
+        }
+    ]
+};
